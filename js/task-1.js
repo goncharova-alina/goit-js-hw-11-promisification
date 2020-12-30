061b213db84778bbf0fa87a8c1275713dce2241a
Alina Goncharova <goncarovaalina206@gmail.com>
2020-12-30 11:45:40 +0200

    task-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,10 @@
-// const delay = ms => {
-//   // Твой код
-// };
+const delay = ms => {
+  // Твой код
+};
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
-
-const promise = new Promise((resolve, reject) => {
-  /*
-   * Эта функция будет вызвана автоматически. В ней можно выполнять
-   * любые асинхронные операции. Когда они завершатся — нужно
-   * вызвать одно из: resolve(результат) при успешном выполнении,
-   * или reject(ошибка) при ошибке.
-   */
-});
-console.log(promise);
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
