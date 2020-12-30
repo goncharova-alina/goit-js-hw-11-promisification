26447d2aa2a990f921ce999a0169a24fbd731496
Alina Goncharova <goncarovaalina206@gmail.com>
2020-12-30 12:49:21 +0200

    task-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,11 @@
 const delay = ms => {
-  // Твой код
+ return new Promise(resolve => {
+   setTimeout(() => {
+     resolve(ms);
+   }, ms)
+ });
 };
+
 
 const logger = time => console.log(`Resolved after ${time}ms`);
 
